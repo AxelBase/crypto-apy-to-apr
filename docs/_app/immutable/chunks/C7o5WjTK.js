af8c5e146b072ce64cbdfd8f411de8942beca294
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-12-19 17:22:34 +0200

    GSC

---
+++ docs/_app/immutable/chunks/C7o5WjTK.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_fexqj4?.base??"/crypto-apy-to-apr",a=globalThis.__sveltekit_fexqj4?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/CBxyVB3T.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_f5g48l?.base??"/crypto-apy-to-apr",a=globalThis.__sveltekit_f5g48l?.assets??s??"";export{a,s as b};
